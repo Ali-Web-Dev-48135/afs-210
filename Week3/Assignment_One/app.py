929a55d0467ed07d765134d409658b65828b9147
Ali Al Dairawi <ali.aldairawi@bryanuniversity.edu>
2023-03-19 21:10:02 -0800

    Updated app.py Linked List Assignment

--- Week3/Assignment_One/app.py
+++ Week3/Assignment_One/app.py
@@ -10,11 +10,9 @@ myList.add('YOU')
 myList.add('!')
 
 print(myList)
-# print('THIS IS THE COUNT', myList.size())
 print(myList.indexOf('WITH'))
 myList.addAtIndex('US', 4)
 myList.addAtIndex('ALL', 5)
 print(myList)
 
-#print(myList.size())
 
